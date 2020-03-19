344acfabe21468f64bae84430010051b1f59fa2c
PWalis <pwilky1016@gmail.com>
2020-03-19 15:16:57 -0700

    createing a flask app and deploying it to heroku

---
+++ Flask_app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+# @app.route("/")
+# def hello():
+#     return "Hello World!"
+
+def create_app():
+
+    app = Flask(__name__)
+
+    @app.route('/')
+    def home():
+        return ('<h1>hello<h1>')
+    
+    return app
